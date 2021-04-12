01a1d1a68ee8459c383113e1cc8a3b1d4d721a5f
raulrod00 <raulrod00@yahoo.com>
2021-04-12 16:52:22 -0700

    Initial Complete
    Can move the platform,  with one command. But there's a rounding issue that won't let 51 mm be used. Allowed the use of separate motor commands

--- HexControl/Form1.Designer.cs
+++ HexControl/Form1.Designer.cs
@@ -40,6 +40,19 @@ namespace Hex_Control
             this.sendCom = new System.Windows.Forms.Button();
             this.retOrigin = new System.Windows.Forms.Button();
             this.Test_String = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.s_xPos = new System.Windows.Forms.TextBox();
+            this.xlabel = new System.Windows.Forms.Label();
+            this.Ylabel = new System.Windows.Forms.Label();
+            this.s_yPos = new System.Windows.Forms.TextBox();
+            this.zlabel = new System.Windows.Forms.Label();
+            this.s_zPos = new System.Windows.Forms.TextBox();
+            this.wlabel = new System.Windows.Forms.Label();
+            this.s_wPos = new System.Windows.Forms.TextBox();
+            this.vlabel = new System.Windows.Forms.Label();
+            this.s_vPos = new System.Windows.Forms.TextBox();
+            this.ulabel = new System.Windows.Forms.Label();
+            this.s_uPos = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // LinearLength
@@ -69,16 +82,16 @@ namespace Hex_Control
             // 
             this.ActLength.Location = new System.Drawing.Point(187, 29);
             this.ActLength.Name = "ActLength";
-            this.ActLength.Size = new System.Drawing.Size(125, 27);
+            this.ActLength.Size = new System.Drawing.Size(59, 27);
             this.ActLength.TabIndex = 3;
-            this.ActLength.Text = "300";
+            this.ActLength.Text = "50";
             this.ActLength.TextChanged += new System.EventHandler(this.ActLength_TextChanged);
             // 
             // RotPulses
             // 
             this.RotPulses.Location = new System.Drawing.Point(187, 95);
             this.RotPulses.Name = "RotPulses";
-            this.RotPulses.Size = new System.Drawing.Size(125, 27);
+            this.RotPulses.Size = new System.Drawing.Size(59, 27);
             this.RotPulses.TabIndex = 5;
             this.RotPulses.Text = "10000";
             // 
@@ -96,7 +109,7 @@ namespace Hex_Control
             // 
             this.RotLength.Location = new System.Drawing.Point(187, 62);
             this.RotLength.Name = "RotLength";
-            this.RotLength.Size = new System.Drawing.Size(125, 27);
+            this.RotLength.Size = new System.Drawing.Size(59, 27);
             this.RotLength.TabIndex = 7;
             this.RotLength.Text = "5";
             // 
@@ -111,11 +124,10 @@ namespace Hex_Control
             // 
             // sendCom
             // 
-            this.sendCom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.sendCom.Location = new System.Drawing.Point(482, 409);
+            this.sendCom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendCom.Location = new System.Drawing.Point(565, 409);
             this.sendCom.Name = "sendCom";
-            this.sendCom.Size = new System.Drawing.Size(620, 29);
+            this.sendCom.Size = new System.Drawing.Size(140, 29);
             this.sendCom.TabIndex = 8;
             this.sendCom.Text = "Send Command";
             this.sendCom.UseVisualStyleBackColor = true;
@@ -124,27 +136,148 @@ namespace Hex_Control
             // retOrigin
             // 
             this.retOrigin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.retOrigin.Location = new System.Drawing.Point(1120, 409);
+            this.retOrigin.Location = new System.Drawing.Point(723, 409);
             this.retOrigin.Name = "retOrigin";
             this.retOrigin.Size = new System.Drawing.Size(144, 29);
             this.retOrigin.TabIndex = 9;
             this.retOrigin.Text = "Return to Origin";
             this.retOrigin.UseVisualStyleBackColor = true;
+            this.retOrigin.Click += new System.EventHandler(this.retOrigin_Click);
             // 
             // Test_String
             // 
             this.Test_String.AutoSize = true;
-            this.Test_String.Location = new System.Drawing.Point(2, 226);
+            this.Test_String.Location = new System.Drawing.Point(12, 344);
             this.Test_String.Name = "Test_String";
             this.Test_String.Size = new System.Drawing.Size(50, 20);
             this.Test_String.TabIndex = 10;
             this.Test_String.Text = "label1";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(289, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(169, 24);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Separate Commands";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // s_xPos
+            // 
+            this.s_xPos.Location = new System.Drawing.Point(86, 158);
+            this.s_xPos.Name = "s_xPos";
+            this.s_xPos.Size = new System.Drawing.Size(52, 27);
+            this.s_xPos.TabIndex = 12;
+            // 
+            // xlabel
+            // 
+            this.xlabel.AutoSize = true;
+            this.xlabel.Location = new System.Drawing.Point(22, 161);
+            this.xlabel.Name = "xlabel";
+            this.xlabel.Size = new System.Drawing.Size(58, 20);
+            this.xlabel.TabIndex = 13;
+            this.xlabel.Text = "X (mm)";
+            // 
+            // Ylabel
+            // 
+            this.Ylabel.AutoSize = true;
+            this.Ylabel.Location = new System.Drawing.Point(166, 161);
+            this.Ylabel.Name = "Ylabel";
+            this.Ylabel.Size = new System.Drawing.Size(57, 20);
+            this.Ylabel.TabIndex = 15;
+            this.Ylabel.Text = "Y (mm)";
+            // 
+            // s_yPos
+            // 
+            this.s_yPos.Location = new System.Drawing.Point(230, 158);
+            this.s_yPos.Name = "s_yPos";
+            this.s_yPos.Size = new System.Drawing.Size(52, 27);
+            this.s_yPos.TabIndex = 14;
+            // 
+            // zlabel
+            // 
+            this.zlabel.AutoSize = true;
+            this.zlabel.Location = new System.Drawing.Point(316, 161);
+            this.zlabel.Name = "zlabel";
+            this.zlabel.Size = new System.Drawing.Size(58, 20);
+            this.zlabel.TabIndex = 17;
+            this.zlabel.Text = "Z (mm)";
+            // 
+            // s_zPos
+            // 
+            this.s_zPos.Location = new System.Drawing.Point(380, 158);
+            this.s_zPos.Name = "s_zPos";
+            this.s_zPos.Size = new System.Drawing.Size(52, 27);
+            this.s_zPos.TabIndex = 16;
+            // 
+            // wlabel
+            // 
+            this.wlabel.AutoSize = true;
+            this.wlabel.Location = new System.Drawing.Point(316, 212);
+            this.wlabel.Name = "wlabel";
+            this.wlabel.Size = new System.Drawing.Size(63, 20);
+            this.wlabel.TabIndex = 23;
+            this.wlabel.Text = "W (mm)";
+            // 
+            // s_wPos
+            // 
+            this.s_wPos.Location = new System.Drawing.Point(380, 209);
+            this.s_wPos.Name = "s_wPos";
+            this.s_wPos.Size = new System.Drawing.Size(52, 27);
+            this.s_wPos.TabIndex = 22;
+            // 
+            // vlabel
+            // 
+            this.vlabel.AutoSize = true;
+            this.vlabel.Location = new System.Drawing.Point(166, 212);
+            this.vlabel.Name = "vlabel";
+            this.vlabel.Size = new System.Drawing.Size(58, 20);
+            this.vlabel.TabIndex = 21;
+            this.vlabel.Text = "V (mm)";
+            // 
+            // s_vPos
+            // 
+            this.s_vPos.Location = new System.Drawing.Point(229, 209);
+            this.s_vPos.Name = "s_vPos";
+            this.s_vPos.Size = new System.Drawing.Size(52, 27);
+            this.s_vPos.TabIndex = 20;
+            // 
+            // ulabel
+            // 
+            this.ulabel.AutoSize = true;
+            this.ulabel.Location = new System.Drawing.Point(21, 212);
+            this.ulabel.Name = "ulabel";
+            this.ulabel.Size = new System.Drawing.Size(59, 20);
+            this.ulabel.TabIndex = 19;
+            this.ulabel.Text = "U (mm)";
+            // 
+            // s_uPos
+            // 
+            this.s_uPos.Location = new System.Drawing.Point(86, 209);
+            this.s_uPos.Name = "s_uPos";
+            this.s_uPos.Size = new System.Drawing.Size(52, 27);
+            this.s_uPos.TabIndex = 18;
+            // 
             // RotationLength
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 450);
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.wlabel);
+            this.Controls.Add(this.s_wPos);
+            this.Controls.Add(this.vlabel);
+            this.Controls.Add(this.s_vPos);
+            this.Controls.Add(this.ulabel);
+            this.Controls.Add(this.s_uPos);
+            this.Controls.Add(this.zlabel);
+            this.Controls.Add(this.s_zPos);
+            this.Controls.Add(this.Ylabel);
+            this.Controls.Add(this.s_yPos);
+            this.Controls.Add(this.xlabel);
+            this.Controls.Add(this.s_xPos);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.Test_String);
             this.Controls.Add(this.retOrigin);
             this.Controls.Add(this.sendCom);
@@ -177,6 +310,19 @@ namespace Hex_Control
         private System.Windows.Forms.Button sendCom;
         private System.Windows.Forms.Button retOrigin;
         private System.Windows.Forms.Label Test_String;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox s_xPos;
+        private System.Windows.Forms.Label xlabel;
+        private System.Windows.Forms.Label Ylabel;
+        private System.Windows.Forms.TextBox s_yPos;
+        private System.Windows.Forms.Label zlabel;
+        private System.Windows.Forms.TextBox s_zPos;
+        private System.Windows.Forms.Label wlabel;
+        private System.Windows.Forms.TextBox s_wPos;
+        private System.Windows.Forms.Label vlabel;
+        private System.Windows.Forms.TextBox s_vPos;
+        private System.Windows.Forms.Label ulabel;
+        private System.Windows.Forms.TextBox s_uPos;
     }
 }
 
--- HexControl/Form1.cs
+++ HexControl/Form1.cs
@@ -27,11 +27,11 @@ namespace Hex_Control
         //Control protocol packet
         private const ushort confirmCode = 0x55aa;
         private const ushort passCode = 0;
-        private const ushort functionCode = 0x1401;
+        private const ushort functionCode = 0x1301;
         private const ushort objectChannel = 1;
-        private const ushort whoAccpet = 0xFFFF;
-        private const ushort whoReply = 0;
-        private const ushort line = 0;
+        private const ushort whoAccept = 0xFFFF;
+        private const ushort whoReply = 0xFFFF;
+        private const ushort UDPverification = 0;
         private uint absTime = 50;//millisecond
         private uint xPos = 1;
         private uint yPos = 1;
@@ -39,12 +39,13 @@ namespace Hex_Control
         private uint uPos = 1;
         private uint vPos = 1;
         private uint wPos = 1;
-        private uint baseDout1 = 0;
-        private uint baseDout2 = 0;
-        private const uint dac = 0;
-        private uint baseDout3 = 0;
-        private uint baseDout4 = 0;
-        private uint effects = 0;
+        //private uint baseDout1 = 0;
+        //private uint baseDout2 = 0;
+        //private const uint dac = 0;
+        //private uint baseDout3 = 0;
+        //private uint baseDout4 = 0;
+        private uint SFXOut = 0x1234;
+        private uint analogOut = 0x5678abcd;
 
         //udp
         private IPEndPoint mboxIPE = null;
@@ -57,20 +58,39 @@ namespace Hex_Control
             leadDistanceUnit   = Convert.ToUInt16(RotLength.Text);
             oneTurnPulseNum    = Convert.ToUInt16(RotPulses.Text);
 
-            uint _xPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * xPos / 255;
-            uint _yPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * yPos / 255;
-            uint _zPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * zPos / 255;
-            uint _uPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * uPos / 255;
-            uint _vPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * vPos / 255;
-            uint _wPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * wPos / 255;
+            uint _xPos;// = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * xPos; // 255;
+            uint _yPos;// = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * yPos; // 255;
+            uint _zPos;// = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * zPos; // 255;
+            uint _uPos;// = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * uPos; // 255;
+            uint _vPos;// = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * vPos; // 255;
+            uint _wPos;// s = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * wPos; // 255;
+
+            if (checkBox1.Checked)
+            {
+                _xPos = (Convert.ToUInt32(s_xPos.Text) / leadDistanceUnit) * oneTurnPulseNum * xPos; // 255;
+                _yPos = (Convert.ToUInt32(s_yPos.Text) / leadDistanceUnit) * oneTurnPulseNum * yPos; // 255;
+                _zPos = (Convert.ToUInt32(s_zPos.Text) / leadDistanceUnit) * oneTurnPulseNum * zPos; // 255;
+                _uPos = (Convert.ToUInt32(s_uPos.Text) / leadDistanceUnit) * oneTurnPulseNum * uPos; // 255;
+                _vPos = (Convert.ToUInt32(s_vPos.Text) / leadDistanceUnit) * oneTurnPulseNum * vPos; // 255;
+                _wPos = (Convert.ToUInt32(s_wPos.Text) / leadDistanceUnit) * oneTurnPulseNum * wPos; // 255;
+            }
+            else
+            {
+                _xPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * xPos; // 255;
+                _yPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * yPos; // 255;
+                _zPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * zPos; // 255;
+                _uPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * uPos; // 255;
+                _vPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * vPos; // 255;
+                _wPos = (accessDistanceUnit / leadDistanceUnit) * oneTurnPulseNum * wPos; // 255;
+            }
 
             str.Append(confirmCode.ToString("X4"));
             str.Append(passCode.ToString("X4"));
             str.Append(functionCode.ToString("X4"));
             str.Append(objectChannel.ToString("X4"));
-            str.Append(whoAccpet.ToString("X4"));
+            str.Append(whoAccept.ToString("X4"));
             str.Append(whoReply.ToString("X4"));
-            str.Append(line.ToString("X8"));
+            str.Append(UDPverification.ToString("X8"));
             str.Append(absTime.ToString("X8"));
             str.Append(_xPos.ToString("X8"));
             str.Append(_yPos.ToString("X8"));
@@ -78,16 +98,18 @@ namespace Hex_Control
             str.Append(_uPos.ToString("X8"));
             str.Append(_vPos.ToString("X8"));
             str.Append(_wPos.ToString("X8"));
-            str.Append(baseDout1.ToString("X2"));
-            str.Append(baseDout4.ToString("X2"));
-            str.Append(dac.ToString("X8"));
-            str.Append(baseDout3.ToString("X2"));
-            str.Append(baseDout2.ToString("X2"));
+            //str.Append(baseDout1.ToString("X2"));
+            //str.Append(baseDout4.ToString("X2"));
+            //str.Append(dac.ToString("X8"));
+            //str.Append(baseDout3.ToString("X2"));
+            //str.Append(baseDout2.ToString("X2"));
+            str.Append(SFXOut.ToString("X4"));
+            str.Append(analogOut.ToString("X8"));
 
             string sendStr = str.ToString().ToLower();
 
             byte[] sendBuf = HexStringToByteArray(sendStr);
-            //socket.SendTo(sendBuf, mboxIPE);
+            socket.SendTo(sendBuf, mboxIPE);
             Test_String.Text = sendStr;
             //Console.WriteLine(sendBuf);
 
@@ -96,6 +118,12 @@ namespace Hex_Control
         public RotationLength()
         {
             InitializeComponent();
+            s_xPos.Enabled = false;
+            s_yPos.Enabled = false;
+            s_zPos.Enabled = false;
+            s_uPos.Enabled = false;
+            s_vPos.Enabled = false;
+            s_wPos.Enabled = false;
             mboxIPE = new IPEndPoint(IPAddress.Parse("192.168.15.255"), 7408);
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         }
@@ -131,8 +159,9 @@ namespace Hex_Control
         /// </summary>
         public void Reset()
         {
-            string str = "55aa000012010002ffffffff000000010000";
+            string str = "55aa000013010001ffffffff000000000000000000000000000000000000000000000000000000000000000012345678abcd";
             byte[] sendBuf = HexStringToByteArray(str);
+            Test_String.Text = str;
             socket.SendTo(sendBuf, mboxIPE);
         }
 
@@ -146,6 +175,32 @@ namespace Hex_Control
                 buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
             }
             return buffer;
+        }
+
+        private void retOrigin_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                s_xPos.Enabled = true;
+                s_yPos.Enabled = true;
+                s_zPos.Enabled = true;
+                s_uPos.Enabled = true;
+                s_vPos.Enabled = true;
+                s_wPos.Enabled = true;
+            } else
+            {
+                s_xPos.Enabled = false;
+                s_yPos.Enabled = false;
+                s_zPos.Enabled = false;
+                s_uPos.Enabled = false;
+                s_vPos.Enabled = false;
+                s_wPos.Enabled = false;
+            }
         }
 
     }
